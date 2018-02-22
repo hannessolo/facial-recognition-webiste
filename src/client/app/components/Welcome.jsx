3cf02f1c7c4c5733b4a6593c8703392986b0d738
Hannes Hertach <hanneshertach@gmail.com>
2018-02-22 23:05:24 +0000

    changed welcome text

--- src/client/app/components/Welcome.jsx
+++ src/client/app/components/Welcome.jsx
@@ -4,7 +4,11 @@ export default class Welcome extends Component {
 
   render() {
     return (
-        <h1>Welcome</h1>
+        <div className='content-container'>
+          <div className='content'>
+            <h1>Welcome</h1>
+          </div>
+        </div>
     )
   }
 
